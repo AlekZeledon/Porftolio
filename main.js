1c5a190aef7656d520505ab966106e94eef3e0d8
Alek Zeledon <alekzeledon@gmail.com>
2021-10-16 04:18:47 -0700

    nav bar fixed

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ $(document).ready(function () {
         $('.nav-container').toggleClass('open');
     });    
 });
+
+$('.nav-container .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.nav-container').removeClass('open');
+});
+
+$('nav a[href*="#"]').on('click', function () {
+   $('html, body').animate({
+       scrollTop: $($(this).attr('href')).offset().top - 100
+   }, 2000);
+});
+
+$('#up').on('click', function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 2000);
+});
+
+AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
+});
